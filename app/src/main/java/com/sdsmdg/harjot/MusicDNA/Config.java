cfb812ec65b04d02a2f81f4a826bc5cb2b955982
soneu03 <84006365+soneu03@users.noreply.github.com>
2024-07-11 13:42:43 +0200

    Create Config.java

---
+++ app/src/main/java/com/sdsmdg/harjot/MusicDNA/Config.java
@@ -0,0 +1,7 @@
+package com.sdsmdg.harjot.MusicDNA;
+
+public class Config {
+    public static final String CLIENT_ID = "YOUR_SOUNDCLOUD_CLIENT_ID";
+    public static final String API_URL = "https://api.soundcloud.com";
+    public static final String GENIUS = "YOUR_GENIUS_API_KEY";
+}
