842a78cdf76d7e743be247bb06c48a9fb4fc8bc6
soneu03 <84006365+soneu03@users.noreply.github.com>
2024-07-12 09:13:10 +0200

    Update Config.java

--- app/src/main/java/com/sdsmdg/harjot/MusicDNA/Config.java
+++ app/src/main/java/com/sdsmdg/harjot/MusicDNA/Config.java
@@ -1,7 +1,10 @@
 package com.sdsmdg.harjot.MusicDNA;
-
+//
+//Replace YOUR_SOUNDCLOUD_CLIENT_ID with a client id received from SoundCloud or leave it blank if you don't want to use SoundCloud streaming.Get SoundCloud Client ID from here : https://developers.soundcloud.com/
+//Replace YOUR_GENIUS_API_KEY with an API key received from Genius or leave it blank if you don't want to use lyrics from genius.com. Get Genius API key from here : https://genius.com/api-clients/new
+//After that just import the project into Android Studio.
 public class Config {
     public static final String CLIENT_ID = "YOUR_SOUNDCLOUD_CLIENT_ID";
     public static final String API_URL = "https://api.soundcloud.com";
-    public static final String GENIUS = "YOUR_GENIUS_API_KEY";
+    public static final String GENIUS = "";
 }
